e8bbc70e848980af333b1a1b31e5bb97c91bb7a3
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-03-02 10:23:25 +0800

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl">NextJS + TailwindCSS Starter </h1>
+    </div>
+  );
+};
+
+export default Home;
